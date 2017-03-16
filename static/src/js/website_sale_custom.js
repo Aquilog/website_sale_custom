7afd9bb418c89f407d982096ace8cee86f3bebe9
Guillaume JULLIEN <g.jullien@aquilog.fr>
2017-03-16 14:52:40 +0100

    modif

--- static/src/js/website_sale_custom.js
+++ static/src/js/website_sale_custom.js
@@ -6,50 +6,53 @@ odoo.define('website_sale_custom', function (require) {
   var core = require('web.core');
   require('website.website');
 
-  var TexturePicker = Widget.extend({
+  var ValuePicker = Widget.extend({
     // template: 'website_sale_custom.index',
     init: function () {
       this.eventType = 'click touchstart';
     },
     start: function () {
-      this.menu_dropdown();
+      // this.menu_dropdown();
       this.action_select();
     },
-    menu_dropdown: function () {
-      $('.texture-dropdown').on(this.eventType, function (e) {
-        e.stopImmediatePropagation();
-        if (!$(this).next().hasClass('is-open')) {
-          $(this).addClass('is-active');
-          $(this).next().addClass('is-open');
-        } else {
-          $(this).removeClass('is-active');
-          $(this).next().removeClass('is-open');
-        }
-      });
-    },
+    // menu_dropdown: function () {
+    //   $('.texture-dropdown').on(this.eventType, function (e) {
+    //     e.stopImmediatePropagation();
+    //     if (!$(this).next().hasClass('is-open')) {
+    //       $(this).addClass('is-active');
+    //       $(this).next().addClass('is-open');
+    //     } else {
+    //       $(this).removeClass('is-active');
+    //       $(this).next().removeClass('is-open');
+    //     }
+    //   });
+    // },
     action_select: function () {
-      $('.texture-row').on(this.eventType, function (e) {
+      $('.variant-value').on(this.eventType, function (e) {
         // e.stopImmediatePropagation();
-        var link = $(this);
-        var icon = link.find('.status:first');
-        var value = link.attr('data-value')
-        var css = link.attr('data-class')
-        $('#texture-text').html(value)
-        $('#texture-preview').removeAttr('class').addClass(css)
-        $('.texture-row').removeClass('is-selected');
-        $('.status').addClass('hide');
-        var span = link.parent().parent().prev();
-        span.find('span.texture-cube').removeAttr('class').addClass('texture-cube ' + css);
-        span.find('span.texture-label').html(value);
-
-
-        link.addClass('is-selected');
-        icon.removeClass('hide')
+        var self = $(this);
+        // var Value = new instance.web.Model('product.attribute.value')
+        // var image_name = Value.read('name').filter([('id', '=', $('#value-img').attr('id'))])
+        // console.log()
+        // var icon = link.find('.status:first');
+        // var value = link.attr('data-value')
+        // var css = link.attr('data-class')
+        // $('#texture-text').html(value)
+        
+        // $('#value-preview').css("visibility", "visible");
+        $('#value-img').attr("src", "/website_sale_custom/static/src/img/seat_colors/cerise.png");
+        // $('.texture-row').removeClass('is-selected');
+        // $('.status').addClass('hide');
+        // var span = link.parent().parent().prev();
+        // span.find('span.texture-cube').removeAttr('class').addClass('texture-cube ' + css);
+        // span.find('span.texture-label').html(value);
+        // link.addClass('is-selected');
+        // icon.removeClass('hide')
       });
     }
   });
 
-  var t = new TexturePicker();
+  var t = new ValuePicker();
   t.start();
   return t;
 });
